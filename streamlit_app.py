427e6dbfeaadb521a3a77c5402a6e74ffc718109
catherinoalbornoz-lgtm <catherino.albornoz@gmail.com>
2026-09-22 16:58:06 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
-import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
 st.text(smoothiefroot_response)
 
 st.title("🥤 Customize Your Smoothie!")
