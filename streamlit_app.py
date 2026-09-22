5db9344f59753f96a02eab7ef38f811cdac60855
catherinoalbornoz-lgtm <catherino.albornoz@gmail.com>
2026-09-22 14:55:59 -0300

    Update streamlit_app.py
    Connected changes needed from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import os
-
 from snowflake.snowpark.functions import col
 
 st.title("🥤 Customize Your Smoothie!")
@@ -9,16 +7,17 @@ st.write("""
 Choose the fruits you want in your custom Smoothie!
 """)
 
-# Crear conexión con Snowflake
+# Conexión con Snowflake
 conn = st.connection("snowflake")
 session = conn.session()
 
-# Obtener frutas desde la tabla
-my_dataframe = session.table(
-    "SMOOTHIES.PUBLIC.FRUIT_OPTIONS"
-).select(col("FRUIT_NAME"))
+# Obtener frutas desde Snowflake
+my_dataframe = (
+    session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS")
+    .select(col("FRUIT_NAME"))
+)
 
-# Convertir FRUIT_NAME a lista
+# Convertir los resultados a una lista
 fruit_rows = my_dataframe.collect()
 
 fruit_list = [
@@ -26,12 +25,19 @@ fruit_list = [
     for row in fruit_rows
 ]
 
+# Elegir ingredientes
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
     fruit_list,
     max_selections=5
 )
 
+# Nombre de la persona
+name_on_order = st.text_input(
+    "Name on order:"
+)
+
+# Construir lista de ingredientes
 if ingredients_list:
 
     ingredients_string = ""
@@ -41,21 +47,36 @@ if ingredients_list:
 
     st.write(ingredients_string)
 
+    # SQL para insertar pedido
     my_insert_stmt = """
-        INSERT INTO SMOOTHIES.PUBLIC.ORDERS (INGREDIENTS)
-        VALUES ('""" + ingredients_string + """','"""+name_on_order+"""')
+        INSERT INTO SMOOTHIES.PUBLIC.ORDERS
+            (INGREDIENTS, NAME_ON_ORDER)
+        VALUES
+            ('""" + ingredients_string + """','""" + name_on_order + """')
     """
 
-    st.write(my_insert_stmt)
-    st.stop()
-
+# Botón
 time_to_insert = st.button("Submit Order")
 
-
-
 if time_to_insert:
-    if ingredients_list:
+
+    if ingredients_list and name_on_order:
+
         session.sql(my_insert_stmt).collect()
-        st.success("Your Smoothie is ordered!", icon="✅")
+
+        st.success(
+            "Your Smoothie is ordered!",
+            icon="✅"
+        )
+
+    elif not name_on_order:
+
+        st.warning(
+            "Please enter your name."
+        )
+
     else:
-        st.warning("Please choose at least one ingredient.")
+
+        st.warning(
+            "Please choose at least one ingredient."
+        )
