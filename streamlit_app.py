5274cca8a1e17140db62adc7ecdf868a35547ed7
catherinoalbornoz-lgtm <catherino.albornoz@gmail.com>
2026-09-22 16:59:02 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import requests
 from snowflake.snowpark.functions import col
-st.text(smoothiefroot_response)
 
 st.title("🥤 Customize Your Smoothie!")
 
@@ -14,12 +13,10 @@ conn = st.connection("snowflake")
 session = conn.session()
 
 # Obtener frutas desde Snowflake
-my_dataframe = (
-    session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS")
-    .select(col("FRUIT_NAME"))
-)
+my_dataframe = session.table(
+    "SMOOTHIES.PUBLIC.FRUIT_OPTIONS"
+).select(col("FRUIT_NAME"))
 
-# Convertir los resultados a una lista
 fruit_rows = my_dataframe.collect()
 
 fruit_list = [
@@ -27,58 +24,15 @@ fruit_list = [
     for row in fruit_rows
 ]
 
-# Elegir ingredientes
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
     fruit_list,
     max_selections=5
 )
 
-# Nombre de la persona
-name_on_order = st.text_input(
-    "Name on order:"
+# API SmoothieFroot
+smoothiefroot_response = requests.get(
+    "https://my.smoothiefroot.com/api/fruit/watermelon"
 )
 
-# Construir lista de ingredientes
-if ingredients_list:
-
-    ingredients_string = ""
-
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + " "
-
-    st.write(ingredients_string)
-
-    # SQL para insertar pedido
-    my_insert_stmt = """
-        INSERT INTO SMOOTHIES.PUBLIC.ORDERS
-            (INGREDIENTS, NAME_ON_ORDER)
-        VALUES
-            ('""" + ingredients_string + """','""" + name_on_order + """')
-    """
-
-# Botón
-time_to_insert = st.button("Submit Order")
-
-if time_to_insert:
-
-    if ingredients_list and name_on_order:
-
-        session.sql(my_insert_stmt).collect()
-
-        st.success(
-            "Your Smoothie is ordered!",
-            icon="✅"
-        )
-
-    elif not name_on_order:
-
-        st.warning(
-            "Please enter your name."
-        )
-
-    else:
-
-        st.warning(
-            "Please choose at least one ingredient."
-        )
+st.text(smoothiefroot_response)
