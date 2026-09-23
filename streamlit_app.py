af00a325d8246e39576c09a5a00567a0b155edd5
catherinoalbornoz-lgtm <catherino.albornoz@gmail.com>
2026-09-23 12:21:46 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import requests
+import pandas as pd
 from snowflake.snowpark.functions import col
+
 
 st.title("🥤 Customize Your Smoothie!")
 
@@ -24,6 +26,10 @@ my_dataframe = session.table(
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
+#Convert to snowpark dataframe to a pandas Dataframe so wecan use the LOC function
+pd_df=my_daframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 fruit_rows = my_dataframe.collect()
 
