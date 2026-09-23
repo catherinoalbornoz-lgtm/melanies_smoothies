9f0fa244e457e003113b6c19a4b1d0132c0f69f5
catherinoalbornoz-lgtm <catherino.albornoz@gmail.com>
2026-09-23 11:33:28 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ if ingredients_list:
         # API SmoothieFroot
         smoothiefroot_response = requests.get(
             "https://my.smoothiefroot.com/api/fruit/watermelon"
-        )'
+        )
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
